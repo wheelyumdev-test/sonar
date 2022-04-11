87344fca60aad9c364f923fada6c8d046bb2b09a
William Yoon <wyoon@cadent.tv>
2022-04-11 12:57:05 -0400

    Revert the changes

--- node.js
+++
@@ -1,6 +0,0 @@
-module.exports.index = async function (req, res) {
-  const value = req.query.value;
-
-  res.setHeader("Set-Cookie", value); // Noncompliant
-  res.cookie("connect.sid", value); // Noncompliant
-};
